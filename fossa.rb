193ded218753eca9a05ddd926ac8806dbeb92bd4
fossabot <engineering@fossa.com>
2019-09-13 10:50:44 -0700

    Brew formula update for fossa-cli version v1.0.7

--- fossa.rb
+++ fossa.rb
@@ -2,16 +2,16 @@
 class Fossa < Formula
   desc "Fast, portable and reliable dependency analysis for any codebase. Supports license & vulnerability scanning for large monoliths. Language-agnostic; integrates with 20+ build systems."
   homepage "https://fossa.com"
-  version "1.0.6"
+  version "1.0.7"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/fossas/fossa-cli/releases/download/v1.0.6/fossa-cli_1.0.6_darwin_amd64.tar.gz"
-    sha256 "bb46ae3b1cdb072ec762ba339cca67068a0767fdce95f97a9c5975411e52450c"
+    url "https://github.com/fossas/fossa-cli/releases/download/v1.0.7/fossa-cli_1.0.7_darwin_amd64.tar.gz"
+    sha256 "de4c15adba24440bf71976dc82795a8f8261f6b2a04584713a580459f3073f13"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/fossas/fossa-cli/releases/download/v1.0.6/fossa-cli_1.0.6_linux_amd64.tar.gz"
-      sha256 "68a9fc5e115f54de2db1e35b87275ce9200a8deae69cad93c59e104558604c45"
+      url "https://github.com/fossas/fossa-cli/releases/download/v1.0.7/fossa-cli_1.0.7_linux_amd64.tar.gz"
+      sha256 "56584e5509db32de9004af4ddd0affe95f2e3915d8f96f9cadd74de725403df5"
     end
   end
 
